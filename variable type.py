449f9062cb8a00e62632cd9dc0dcca9348751024
onioneyedninja <128801858+onioneyedninja@users.noreply.github.com>
2023-09-11 20:20:56 +0530

    committing file

---
+++ variable type.py
@@ -0,0 +1,19 @@
+# Define variables with different data types
+integer_variable = 42
+float_variable = 3.14
+string_variable = "Hello, World!"
+list_variable = [1, 2, 3]
+tuple_variable = (4, 5, 6)
+dictionary_variable = {"name": "Prabhat", "age": 20}
+boolean_variable = True
+none_variable = None
+
+# Check and display the data types
+print(f"Data Type of integer_variable: {type(integer_variable)}")
+print(f"Data Type of float_variable: {type(float_variable)}")
+print(f"Data Type of string_variable: {type(string_variable)}")
+print(f"Data Type of list_variable: {type(list_variable)}")
+print(f"Data Type of tuple_variable: {type(tuple_variable)}")
+print(f"Data Type of dictionary_variable: {type(dictionary_variable)}")
+print(f"Data Type of boolean_variable: {type(boolean_variable)}")
+print(f"Data Type of none_variable: {type(none_variable)}")
