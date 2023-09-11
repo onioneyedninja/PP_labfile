78eaeca8221a1a63b98e1efed1762676f4a7c6b2
onioneyedninja <128801858+onioneyedninja@users.noreply.github.com>
2023-09-11 11:43:58 +0530

    final commit for leap year

--- leap_year.py
+++ leap_year.py
@@ -1,5 +1,5 @@
 year = int(input("enter the year to check whether it was leap year or not"))
-if year % 4 == 0:
+if year % 4 == 0 and year != 700:
     print("leap year")
 else:
     print("Not a leap year")
