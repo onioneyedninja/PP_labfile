664a3902b52a2b183c760babb8865a5cc3b8d010
onioneyedninja <128801858+onioneyedninja@users.noreply.github.com>
2023-09-11 16:14:21 +0530

    committing file

--- stringSlicing.py
+++
@@ -1,4 +0,0 @@
-a = "slicing"
-print(a[:5])
-print(a[2:5])
-print(a[-4:len(a)])
---
+++ stringSlicingconcatrepititon.py
@@ -0,0 +1,11 @@
+a = "slicing"
+print(a[:5])
+print(a[2:5])
+print(a[-4:len(a)])
+
+b = "python"
+c = "programming"
+print(b + c)  # concatenation
+
+d = "Radhe"
+print(d * 2)  # repetition
