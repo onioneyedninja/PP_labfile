1d9e95bf7252166dbcb3a492cbed6e9d9172f243
onioneyedninja <128801858+onioneyedninja@users.noreply.github.com>
2023-09-11 16:20:02 +0530

    committing file

---
+++ area of circle.py
@@ -0,0 +1,4 @@
+pi = 3.1416
+r = float(input("Enter radius of the circle"))
+area = pi * (r ** 2)
+print("the area of circle is ", area, "Units")
--- stringSlicingconcatrepititon.py
+++ stringSlicingconcatrepititon.py
@@ -3,9 +3,9 @@ print(a[:5])
 print(a[2:5])
 print(a[-4:len(a)])
 
-b = "python"
+b = "python "
 c = "programming"
 print(b + c)  # concatenation
 
-d = "Radhe"
+d = "Radhe "
 print(d * 2)  # repetition
