b1bbb031a7b74b99643496b70d550879b3a0393b
onioneyedninja <128801858+onioneyedninja@users.noreply.github.com>
2023-09-15 14:25:56 +0530

    committing file

---
+++ piecharts.py
@@ -0,0 +1,6 @@
+from numpy import array
+from matplotlib.pyplot import pie, show
+
+data_array = ([3.5, 2.5, 2.5, 3.5])
+pie(data_array)
+show()
---
+++ pyplot.py
@@ -0,0 +1,9 @@
+import numpy
+import matplotlib.pyplot
+from matplotlib.pyplot import show
+import numpy as np
+
+x = [30, 45, 60, 90]
+y = numpy.sin(x)
+matplotlib.pyplot.plot(x, y)
+show()
